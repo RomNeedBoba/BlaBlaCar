c2f3ac9d14d4fb6e4989a213366339d6872be51e
Rom Jr. <157264890+RomNeedBoba@users.noreply.github.com>
2025-03-29 01:36:42 +0700

    Delete lib directory

--- lib/repository/LocationsRepository.dart
+++
@@ -1,5 +0,0 @@
-import '../model/ride/locations.dart';
-
-abstract class LocationsRepository {
-  List<Location> getLocations();
-}
--- lib/repository/ride_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../model/ride/ride.dart';
-import '../model/ride_pref/ride_pref.dart';
-import '../service/rides_service.dart';
-
-abstract class RidesRepository {
-  List<Ride> getRides(RidePreference preference, RidesFilter? filter, RideSortType? sortType);
-}
